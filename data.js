b06979f1159f164dfe208249c3c02ecb1fda934c
SYouper <berk26ozturk@gmail.com>
2026-03-17 19:02:12 +0300

    Add files via upload

--- data.js
+++ data.js
@@ -7,7 +7,7 @@ const mySpots = [
     "Çalışma",
     "İstasyon Caddesi'nde, geniş masaları ve hızlı internetiyle öğrencilerin favorisi.",
     "₺₺",
-    ["images/coffeemania.webp", "images/coffeemania.jpeg"], // Fotoğraf yoksa otomatik AI yer tutucu çalışır
+    ["images/coffeemania.webp", "images/coffeemania.jpeg"],
     [
       "☕ En İyi Kahve",
       "🔌 Her Masada Priz",
